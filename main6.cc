78258c5e77e2b3f37dade24254f68caeab07576f
Andreiiii19 <97831622+Andreiiii19@users.noreply.github.com>
2023-03-10 01:24:23 +0100

    Add files via upload

---
+++ main6.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void leerLinea1(ifstream& infile, string& linea) {
+    string elemento;
+    linea = "";
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    linea += elemento + ';';
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento, ';');
+    getline(infile, elemento);
+    linea += elemento;
+}
+
+int main(){
+    string linea;
+    string namefile = "LigaHost.csv";
+    string namefich = "OtrosNombres.csv";
+    ifstream infile(namefile);
+    ofstream outfile(namefich);
+    if(!infile || !outfile) {
+        cerr  << "NO SE PUDO ABRIR LOS ARCHIVOS" << endl;
+        return 1;
+    }
+    leerLinea1(infile, linea);
+    while(!infile.eof()){
+        leerLinea1(infile, linea);
+        outfile << linea << endl;
+    }
+    infile.close();
+    outfile.close();
+    return 0;
+}
