0bcd0d95f8e38de528f4c2d40664d27d22a6a280
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-03-11 15:59:49 +0100

    jornada

--- data/jornadas.sh
+++ data/jornadas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat LigaHost.csv | cut -d ';' -f 5,19-20  | sort | uniq  >jornada.csv
+cat LigaHost.csv | cut -d ';' -f 5,19-20,22 | cut -d '"' -f 1  | sort | uniq  >jornada.csv
--- data/temporadas.sh
+++ data/temporadas.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cat LigaHost.csv | cut -d ';' -f 1-3,19,22 | cut -d '"' -f 1  | sort | uniq  >temporada.csv
