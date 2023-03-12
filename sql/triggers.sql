634707f08c5bfea11475a8342fb2c945dc9a1882
Andreiiii19 <97831622+Andreiiii19@users.noreply.github.com>
2023-03-12 18:15:26 +0100

    tercer trigger que se crea con errores compilacion

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -8,7 +8,7 @@ FOR EACH ROW
       de ser menor o igual a la de fin');
   END IF;
   END check_annos;
-/
+/ 
   
 /*Trigger que evita que se introduzcan dos veces el mismo equipo en un partido de la misma jornada*/
 CREATE OR REPLACE TRIGGER check_jornada
@@ -25,4 +25,18 @@ BEGIN
       raise_application_error(-20004, 'Un equipo no puede jugar más de una vez en una jornada');
       END IF;
 END check_jornada;
+/ 
+
+
+/*Trigger que evalua que el estadio del partido sea el del local*/
+CREATE OR REPLACE TRIGGER check_estadio
+BEFORE INSERT ON PARTIDOS
+FOR EACH ROW
+BEGIN  
+  IF ((SELECT COUNT(*) FROM EQUIPOS eq WHERE eq.nombreCorto = :new.equipoLocal
+      AND (:new.estadio is null OR :new.estadio != eq.nomEstadio)) > 0) THEN
+          SELECT E.nomEstadio INTO :new.estadio FROM EQUIPOS E
+          WHERE E.nombreCorto = :new.equipoLocal;
+  END IF;
+END check_estadio;
 /
